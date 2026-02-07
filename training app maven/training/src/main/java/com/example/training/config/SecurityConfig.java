7915b19bf399b5dac7f9a4ff9799c39dc3842d6e
sterownik <4golda4@gmail.com>
2026-02-07 16:01:30 +0100

    add variable

--- training app maven/training/src/main/java/com/example/training/config/SecurityConfig.java
+++ training app maven/training/src/main/java/com/example/training/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/",
                                 "/index.html",
+                                "/welcome",
                                 "/*.js",
                                 "/*.css",
                                 "/*.ico",

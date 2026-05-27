081ea2820041745383d68b8fb9da3b684a93e9a7
wojciechgolda <4golda4@gmail.com>
2026-05-27 21:43:42 +0200

    fix

--- training app maven/training/src/main/java/com/example/training/model/ReadyToSendAi.java
+++ training app maven/training/src/main/java/com/example/training/model/ReadyToSendAi.java
@@ -28,8 +28,32 @@ public class ReadyToSendAi {
     public static class AthleteContext {
         public final String sport_background = "cycling + running";
         public final String goal =  "general fitness / FTP improvement";
-        public final String experience_level = "próg przemian tlenowych 200 watt 168 bpm, beztletnowych 250watt 189 bpm" +
-                ", hemoglobina 16,7 g/dl, hemokryt 48%, vo2max 55, waga 67kg";
+        public final String experience_level = "SPORTOWIEC:\n" +
+                "- wiek: 30\n" +
+                "- wzrost: 173 cm\n" +
+                "- masa: 68 kg\n" +
+                "\n" +
+                "WYDOLNOŚĆ:\n" +
+                "- VO2max: 55.2 ml/kg/min\n" +
+                "- FTP/prog anaerobowy (AT): 250 W\n" +
+                "- FTP względne: 3.68 W/kg\n" +
+                "- próg tlenowy (LT): 200 W\n" +
+                "- moc maksymalna: 320 W\n" +
+                "- HR max: 205\n" +
+                "\n" +
+                "STREFY:\n" +
+                "- recovery: <150 W\n" +
+                "- low intensity: 150–200 W\n" +
+                "- middle intensity: 200–225 W\n" +
+                "- high intensity: 225–250 W\n" +
+                "- very high intensity: >250 W\n" +
+                "\n" +
+                "CHARAKTERYSTYKA:\n" +
+                "- dobra wydolność tlenowa\n" +
+                "- dobry potencjał wysiłków wytrzymałościowych\n" +
+                "- mocny metabolizm beztlenowy\n" +
+                "- dobra regeneracja mleczanu\n" +
+                "- cel: poprawa FTP";
 
         public AthleteContext() {
         }
